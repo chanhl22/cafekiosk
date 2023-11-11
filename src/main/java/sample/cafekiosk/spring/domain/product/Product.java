554f16c7174a2fc9d868fede7527dc9ddb30ee69
chanhl22 <lch9502@naver.com>
2023-11-11 10:00:23 +0900

    feat: JPA Auditing (BaseEntity 구성)

--- src/main/java/sample/cafekiosk/spring/domain/product/Product.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -3,6 +3,7 @@ package sample.cafekiosk.spring.domain.product;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.domain.BaseEntity;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -14,7 +15,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Product {
+public class Product extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
