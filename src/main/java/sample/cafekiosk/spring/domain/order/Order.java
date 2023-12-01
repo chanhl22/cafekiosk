97bb26a573797eed7556fa50f556d2bb0c02296f
chanhl22 <lch9502@naver.com>
2023-12-01 22:11:32 +0900

    feat: Order 엔티티 & OrderProduct 엔티티 추가

--- src/main/java/sample/cafekiosk/spring/domain/order/Order.java
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -4,15 +4,20 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProduct;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -30,5 +35,8 @@ public class Order extends BaseEntity {
     private int totalPrice;
 
     private LocalDateTime registeredDateTime;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
 
 }
