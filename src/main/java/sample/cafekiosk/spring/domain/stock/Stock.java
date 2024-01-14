54779cf8fd60e7b9bcecd6a66c2474c79fb97a08
chanhl22 <lch9502@naver.com>
2024-01-14 15:55:27 +0900

    feat: 재고 차감

--- src/main/java/sample/cafekiosk/spring/domain/stock/Stock.java
+++ src/main/java/sample/cafekiosk/spring/domain/stock/Stock.java
@@ -40,4 +40,12 @@ public class Stock extends BaseEntity {
     public boolean isQuantityLessThan(int quantity) {
         return this.quantity < quantity;
     }
+
+    public void deductQuantity(int quantity) {
+        if(isQuantityLessThan(quantity)) {
+            throw new IllegalArgumentException("차감할 재고 수량이 없습니다.");
+        }
+        this.quantity -= quantity;
+    }
+
 }
