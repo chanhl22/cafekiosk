987c2ec7bc2ce094675aa16ff014903a42edf3df
chanhl22 <lch9502@naver.com>
2024-02-12 19:03:40 +0900

    test: 로그 추가 및 static import

--- src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
+++ src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
@@ -8,12 +8,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
-import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
-import sample.cafekiosk.spring.domain.product.ProductType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
 @WebMvcTest(controllers = ProductController.class)
 class ProductControllerTest {
@@ -32,18 +34,19 @@ class ProductControllerTest {
     void createProduct() throws Exception {
         //given
         ProductCreateRequest request = ProductCreateRequest.builder()
-                .type(ProductType.HANDMADE)
-                .sellingStatus(ProductSellingStatus.SELLING)
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
                 .name("아메리카노")
                 .price(4000)
                 .build();
 
         //when //then
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/products/new")
+        mockMvc.perform(post("/api/v1/products/new")
                         .content(objectMapper.writeValueAsString(request))
                         .contentType(MediaType.APPLICATION_JSON)
                 )
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
 }
