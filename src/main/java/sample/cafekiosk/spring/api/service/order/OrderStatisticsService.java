b2e0a155d3e915a2caf986b7057f073852059655
chanhl22 <lch9502@naver.com>
2024-03-07 18:36:31 +0900

    feat: 해당 일자에 결제완료된 주문들 가져오기

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsService.java
@@ -2,15 +2,26 @@ package sample.cafekiosk.spring.api.service.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.domain.order.Order;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.order.OrderStatus;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class OrderStatisticsService {
 
-    public void sendOrderStatisticsMail(LocalDateTime orderDate, String email) {
+    private final OrderRepository orderRepository;
+
+    public void sendOrderStatisticsMail(LocalDate orderDate, String email) {
         // 해당 일자에 결제완료된 주문들을 가져와서
+        List<Order> orders = orderRepository.findOrdersBy(
+                orderDate.atStartOfDay(),
+                orderDate.plusDays(1).atStartOfDay(),
+                OrderStatus.PAYMENT_COMPLETED
+        );
 
         // 총 매출 합계를 계산하고
 
