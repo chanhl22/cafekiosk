4b78e71f15ca33d1e388a33abd98fa2327bfc78b
chanhl22 <lch9502@naver.com>
2024-03-26 22:33:53 +0900

    test: sendOrderStatisticsMail (메일 전송 예외 터짐)

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 import sample.cafekiosk.spring.domain.order.Order;
 import sample.cafekiosk.spring.domain.order.OrderRepository;
 import sample.cafekiosk.spring.domain.order.OrderStatus;
@@ -36,18 +38,22 @@ class OrderStatisticsServiceTest {
     @Autowired
     private OrderProductRepository orderProductRepository;
 
+    @Autowired
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
     @AfterEach
     void tearDown() {
         orderProductRepository.deleteAllInBatch();
         orderRepository.deleteAllInBatch();
         productRepository.deleteAllInBatch();
+        mailSendHistoryRepository.deleteAllInBatch();
     }
 
     @DisplayName("결제완료 주문들을 조회하여 매출 통계 메일을 전송한다.")
     @Test
     void sendOrderStatisticsMail() {
         //given
-        LocalDateTime now = LocalDateTime.of(2024, 3, 5, 10, 0);
+        LocalDateTime now = LocalDateTime.of(2024, 3, 5, 0, 0);
 
         Product product1 = createProduct(HANDMADE, "001", 1000);
         Product product2 = createProduct(HANDMADE, "002", 2000);
@@ -55,16 +61,21 @@ class OrderStatisticsServiceTest {
         List<Product> products = List.of(product1, product2, product3);
         productRepository.saveAll(products);
 
-        Order order1 = createPaymentCompletedOrder(LocalDateTime.of(2023, 3, 5, 23, 59, 59), products);
+        Order order1 = createPaymentCompletedOrder(LocalDateTime.of(2024, 3, 4, 23, 59, 59), products);
         Order order2 = createPaymentCompletedOrder(now, products);
-        Order order3 = createPaymentCompletedOrder(LocalDateTime.of(2023, 3, 5, 23, 59, 59), products);
-        Order order4 = createPaymentCompletedOrder(LocalDateTime.of(2023, 3, 6, 0, 0, 0), products);
+        Order order3 = createPaymentCompletedOrder(LocalDateTime.of(2024, 3, 5, 23, 59, 59), products);
+        Order order4 = createPaymentCompletedOrder(LocalDateTime.of(2024, 3, 6, 0, 0, 0), products);
 
         //when
         boolean result = orderStatisticsService.sendOrderStatisticsMail(LocalDate.of(2023, 3, 5), "test@test.com");
 
         //then
         assertThat(result).isTrue();
+
+        List<MailSendHistory> histories = mailSendHistoryRepository.findAll();
+        assertThat(histories).hasSize(2)
+                .extracting("content")
+                .contains("총 매출 합계는 18000원입니다.");
     }
 
     private Order createPaymentCompletedOrder(LocalDateTime now, List<Product> products) {
