00be7c5cfb05b8ad8824da9de1e3fedb3183d284
chanhl22 <lch9502@naver.com>
2024-04-03 11:18:43 +0900

    test: Mockito 리팩토링

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -2,29 +2,73 @@ package sample.cafekiosk.spring.api.service.mail;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
+
+    @Mock
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
 
     @DisplayName("메일 전송 테스트")
     @Test
-    void sendMail() {
+    void sendMail_1() {
         //given
         MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
         MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
 
         MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
 
+        when(mailSendClient.sendEmail(any(String.class), any(String.class), any(String.class), any(String.class)))
+                .thenReturn(true);
+
+        //when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        //then
+        assertThat(result).isTrue();
+        Mockito.verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+    }
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail_2() {
+        //given
+        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+
+        when(mailSendClient.sendEmail(any(String.class), any(String.class), any(String.class), any(String.class)))
+                .thenReturn(true);
+
+        //when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        //then
+        assertThat(result).isTrue();
+        Mockito.verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+    }
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail_3() {
+        //given
         when(mailSendClient.sendEmail(any(String.class), any(String.class), any(String.class), any(String.class)))
                 .thenReturn(true);
 
