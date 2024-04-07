11d3358beb179925c526425ead5ce177f51b543b
chanhl22 <lch9502@naver.com>
2024-04-07 10:10:00 +0900

    test: 테스트간 독립성이 보장되지 않은 코드

--- src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
@@ -8,6 +8,34 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StockTest {
 
+    private static final Stock stock = Stock.create("001", 1);
+
+    @DisplayName("재고의 수량이 제공된 수량보다 적은지 확인한다.")
+    @Test
+    void isQuantityLessThanEx() {
+        //given
+        int quantity = 2;
+
+        //when
+        boolean result = stock.isQuantityLessThan(quantity);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("재고를 주어진 개수만큼 차감할 수 있다.")
+    @Test
+    void deductQuantityEx() {
+        //given
+        int quantity = 1;
+
+        //when
+        stock.deductQuantity(quantity);
+
+        //then
+        assertThat(stock.getQuantity()).isZero();
+    }
+
     @DisplayName("재고의 수량이 제공된 수량보다 적은지 확인한다.")
     @Test
     void isQuantityLessThan() {
